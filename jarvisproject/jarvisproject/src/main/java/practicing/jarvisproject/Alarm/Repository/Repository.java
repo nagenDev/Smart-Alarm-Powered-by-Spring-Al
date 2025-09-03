0e1a417903f0fd71dfa909393af64e1bf6425133
nagenDev <nagendrayounger@gmail.com>
2025-09-03 18:48:37 +0530

    Delete jarvisproject/jarvisproject directory

--- jarvisproject/jarvisproject/src/main/java/practicing/jarvisproject/Alarm/Repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package practicing.jarvisproject.Alarm.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import practicing.jarvisproject.Alarm.Entity.Entity;
-@org.springframework.stereotype.Repository
-public interface Repository extends MongoRepository<Entity,Long> {
-}
